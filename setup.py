31ced75f409fc2c2a2054d31893d933dc20d790c
Ido Moshe Silverwater <ido@agh2o>
2020-09-29 11:49:02 -0400

    py package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deptree',
+    version='0.0.1',
+    description='',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
